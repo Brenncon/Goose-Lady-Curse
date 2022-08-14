0c5fa19e7f7f6023b6bed8d3fd4cd474dac911e3
Qiao <qiaokuanghua@gmail.com>
2022-08-13 22:10:36 -0400

    UI revamp, still need end screen pic

--- Assets/Connors Work/Connors Scripts/Resource Spawner/Spawner.cs
+++ Assets/Connors Work/Connors Scripts/Resource Spawner/Spawner.cs
@@ -15,7 +15,7 @@ public class Spawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+        SpawnObject();
     }
 
     //spawn the object 
