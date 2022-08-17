e0805544f3107e7f9ef83d087045e43ef269afd1
Qiao <qiaokuanghua@gmail.com>
2022-08-17 13:14:44 -0400

    intro camera

--- Assets/Game/Scripts/Player/Controller/Player States/PlayerIntroState.cs
+++ Assets/Game/Scripts/Player/Controller/Player States/PlayerIntroState.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class PlayerIntroState : StateMachineBehaviour
+public class PlayerIntroState : PlayerBaseState
 {
+    public InputReader reader;
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        
+    { 
+        reader.DisableAllInput();
     }
 
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        reader.EnableGameplayInput();
     }
 }
